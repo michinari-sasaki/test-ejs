41d845f4f035ff8dddc417c802c2d1c8d047f064
佐々木道成 <sasakimichinari@sasakimichinarinoMacBook-Air.local>
2021-08-11 15:35:48 +0900

    udpate ejsfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 const gulp = require('gulp');
-const ejs = require('gulp-ejs');
 const rename = require('gulp-rename');
+const ejs = require('gulp-ejs');
+const replace = require('gulp-replace');
+const plumber = require('gulp-plumber');
 
 //EJS(テンプレートエンジン)
-gulp.task('watch', function () {
-  gulp.watch(['src/ejs/**/*.ejs', '!' + 'src/ejs/**/_*.ejs'], gulp.task('ejs'));
-});
-
 gulp.task('ejs', function (done) {
   gulp
     .src(
       ['src/ejs/**/*.ejs', '!' + 'src/ejs/**/_*.ejs'] //参照するディレクトリと出力を除外するファイル
     )
+    .pipe(plumber())
     .pipe(ejs())
-    .pipe(rename({ extname: '.html' })) //拡張子をhtmlに
+    .pipe(rename({
+      extname: '.html'
+    })) //拡張子をhtmlに
     .pipe(gulp.dest('dest/')); //出力先
-
   done();
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['src/ejs/**/*.ejs'], gulp.task('ejs'));
 });
